8b8a91494ecf1e8f5de1d3ef3e157b9a717a7a76
maie421 <maie421@naver.com>
2020-11-09 21:43:17 +0900

    PostRepository 작성

---
+++ src/main/java/book/bookapi/repository/PostRepository.java
@@ -0,0 +1,32 @@
+package book.bookapi.repository;
+
+import book.bookapi.domain.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Post post){
+        em.persist(post);
+    }
+
+    public List<Post> findAll(){
+        return em.createQuery("select p from Post m")
+                .getResultList();
+    }
+
+    public Post findOne(Long id){
+        return em.find(Post.class,id);
+    }
+
+    
+}
